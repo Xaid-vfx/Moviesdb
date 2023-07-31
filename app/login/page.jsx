2561e15ddc57f99ed6d14c267af06e9c3f24a176
Mohd Zaid <mohdzaid@Mohds-MacBook-Air.local>
2023-07-31 17:48:11 +0530

    Test SEO

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import Login from '../../components/login'
 
-export const metadata = {
-  title : 'Login',
-  description: 'Login to Explore the most latest and popular movies and TV shows',
-  alternates: {
-    canonical: '/',
-    languagues: {
-      'en-US' : '/en-US'
-    }
-  }
-}
+
 
 export default function LoginPage() {
   return (
