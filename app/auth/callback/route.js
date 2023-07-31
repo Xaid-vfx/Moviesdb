f1887ec0c56bf91d1b89bcba15cd841c7e6a4265
Mohd Zaid <mohdzaid@Mohds-MacBook-Air.local>
2023-07-31 17:13:22 +0530

    Bug fixes

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -2,7 +2,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-export async function GET(req) {
+export default async function GET(req) {
   const supabase = createRouteHandlerClient({ cookies })
   const { searchParams } = new URL(req.url)
   const code = searchParams.get('code')
